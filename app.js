08149dad41b51f3ae7bea8d9069d612728ae5870
Anguandia <mikeanguandia@gmail.com>
2019-05-15 06:18:51 +0300

    revert

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 var loansRouter = require('./routes/loans');
-var auth = require('../utils/auth');
+var auth = require('./utils/auth');
 
 var app = express();
 
